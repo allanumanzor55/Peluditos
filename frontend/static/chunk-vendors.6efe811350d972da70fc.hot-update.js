7872b284f7f9d2db67ac9d0404ceb845efda24a8
allanumanzor55 <68833929+allanumanzor55@users.noreply.github.com>
2021-11-09 11:51:25 -0600

    Allan
    reparacion 1

--- frontend/static/chunk-vendors.6efe811350d972da70fc.hot-update.js
+++
@@ -1,9 +0,0 @@
-webpackHotUpdate("chunk-vendors",{
-
-/***/ "./node_modules/@babel/runtime/helpers/esm/asyncToGenerator.js":
-false,
-
-/***/ "./node_modules/regenerator-runtime/runtime.js":
-false
-
-})
