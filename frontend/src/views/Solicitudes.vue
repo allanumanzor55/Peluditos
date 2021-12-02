5af7315129c266c5316c9ab440154d09c0f502ee
AntonioP77 <perezmejiajoseantonio@gmail.com>
2021-12-01 20:38:30 -0600

    View Solicitudes

--- frontend/src/views/Solicitudes.vue
+++ frontend/src/views/Solicitudes.vue
@@ -1,10 +1,43 @@
 <template>
     <div>
         <NavbarHome />
-        <v-container fluid class="pink lighten-3">
-            <v-icon>
-                mdi-account-arrow-down
-            </v-icon>
+        <v-container fluid class="amber">
+           <v-expansion-panels focusable popout>
+                <v-expansion-panel
+                    v-for="(item,i) in 12"
+                    :key="i"
+                >
+                    <v-expansion-panel-header>
+                        
+                        <v-img
+                            class="pa-7 secondary rounded-circle d-inline-block mx-2"
+                            lazy-src="https://picsum.photos/id/11/10/6"
+                            max-height="75"
+                            max-width="75"
+                            src="https://picsum.photos/id/11/500/300"
+                        ></v-img>
+
+                        <h5>Titulo de la Solicitud</h5>
+                    </v-expansion-panel-header>
+
+                    <v-expansion-panel-content> 
+                        <v-row class="m-3">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid sint est sunt quidem, repellat distinctio consequuntur quod vero, laboriosam porro recusandae nostrum hic quas debitis deserunt similique exercitationem fugiat incidunt aut ab facere molestiae adipisci ipsa? Provident ullam modi, accusantium commodi in incidunt soluta doloremque earum corporis, ea, officiis deleniti.
+                        </v-row>
+                        <v-row class="m-2" justify="end">
+                            <v-btn class="m-2" depressed color="amber" >
+                                Opcion 1
+                            </v-btn>
+
+                            <v-btn class="m-2" depressed color="amber" >
+                                Opcion 2
+                            </v-btn>
+
+                        </v-row>
+                    </v-expansion-panel-content>
+                    
+                </v-expansion-panel>
+            </v-expansion-panels>
         </v-container>
     </div>
 </template>
